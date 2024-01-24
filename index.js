f1a0eb752fd23d5f44887d1795fdf6997654e809
jonathanwillisdesign <jonathanw@bigmotive.com>
2024-01-24 20:30:00 +0000

    added cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors')
 const PORT = 3000;
 
 const app = express()
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   console.log("Running")
