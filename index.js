b97aeb3a7900add8976963f35174c0da1d960475
jonathanwillisdesign <jonathanw@bigmotive.com>
2024-01-29 10:16:17 +0000

    Setup Post Request Rotue

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const cors = require('cors')
+const bodyParser = require('body-parser'); // Import body-parser
+
 const PORT = 3000;
 
 const app = express()
 
 app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
   console.log("Running")
-  res.json({message: "Server Working"})
+  res.json({"response": "ready"})
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.json({"response": "sent", "name": req.body.name})
 })
 
 app.listen(PORT)
