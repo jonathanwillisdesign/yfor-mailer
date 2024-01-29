3b8347f3d0be6a5b61b68645a530db02e8a58f48
jonathanwillisdesign <jonathanw@bigmotive.com>
2024-01-29 11:08:54 +0000

    Finished post request and sketched out nodemailer integration

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors')
-const bodyParser = require('body-parser'); // Import body-parser
+const bodyParser = require('body-parser');
+const nodemailer = require("nodemailer");
 
-const PORT = 3000;
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "",
+    pass: "",
+  },
+});
+
+async function newEnquiryNotificationEmail({email, name, message}) {
+  const info = await transporter.sendMail({
+    from: {
+      name: name,
+      address: email
+    },
+    to: process.env.NOTIFICATION_EMAIL,
+    subject: `New Enquiry - ${name}`,
+    html: `<p>New enquiry from ${name}</p><br><p>Email - ${email}</p><br><p>${message}</p>`,
+  });
+
+  console.log("Message sent: %s", info.messageId);
+}
+
+const PORT = process.env.PORT || 3000;
 
 const app = express()
 
@@ -16,7 +43,15 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-  console.log(req.body)
+  const { email, name, message } = req.body;
+
+  if (!email || !name || !message) {
+    res.status(400).json({"response": "incomplete"});
+    return;
+  }
+  
+  newEnquiryNotificationEmail({email, name, message}).catch(console.error())
+
   res.json({"response": "sent", "name": req.body.name})
 })
 
